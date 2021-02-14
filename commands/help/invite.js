1e884601bdbe951ffe543bd356c4331f1bd327d7
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-14 17:08:19 +0530

    Update invite.js

--- commands/help/invite.js
+++ commands/help/invite.js
@@ -1,20 +1,20 @@
-const discord = require("discord.js");
+// const discord = require("discord.js");
 
-module.exports = {
-  name: "invite",
-  category: "info",
-  description: "INVITE Automodv12 beta BOT",
-  run: async (client, message, args) => {
+// module.exports = {
+//   name: "invite",
+//   category: "info",
+//   description: "INVITE Automodv12 beta BOT",
+//   run: async (client, message, args) => {
     
-    let embed = new discord.MessageEmbed()
-    .setTitle(`Sorry!`)
-    .setDescription(`I was developed to serve Young Terminator Esports. I cannot be invited to any other servers!`)
-    .setColor("RANDOM")
-    .setFooter(`Dev. XavierxCode`)
-    .setTimestamp(message.timestamp = Date.now())
+//     let embed = new discord.MessageEmbed()
+//     .setTitle(`Sorry!`)
+//     .setDescription(`I was developed to serve Young Terminator Esports. I cannot be invited to any other servers!`)
+//     .setColor("RANDOM")
+//     .setFooter(`Dev. XavierxCode`)
+//     .setTimestamp(message.timestamp = Date.now())
     
-    message.channel .send(embed)
+//     message.channel .send(embed)
     
   
-  }
-}
+//   }
+// }
