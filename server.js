534a9da27af4f00858e1e45f849a01c254fd03ec
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-14 18:46:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,37 +54,6 @@ client.on("message", async message => {
 });
 
 
-client.on("guildMemberAdd", async member => {
-
-  let chx = db.get(`welchannel_${member.guild.id}`);
-
-  if (chx === null) {
-
-    return;
-
-  }
-
-  
-
-   let data = await canva.welcome(member, { link: "https://i.pinimg.com/originals/f3/1c/39/f31c39d56512dc8fbf30f9d0fb3ee9d3.jpg" })
-
- 
-
-    const attachment = new discord.MessageAttachment(
-
-      data,
-
-      "welcome-image.png"
-
-    );
-
-  
-
-  
-
-  client.channels.cache.get(chx).send("Welcome to our Server " + member.user.username, attachment);
-
-});
 
 //chatbot
 
