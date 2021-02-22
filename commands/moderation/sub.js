891a969cbe312535ea48705e8170edfefa14c96a
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-22 13:23:20 +0530

    Update sub.js

--- commands/moderation/sub.js
+++ commands/moderation/sub.js
@@ -8,14 +8,24 @@ module.exports = {
     return
     const RegClosed = new Discord.MessageEmbed()
 	.setColor('#FA780B')
-	.setTitle('Registration closed 🚧')
-	.setDescription('Subscription slots are available. No daily registration required!')
-  .setImage('https://cdn.discordapp.com/attachments/734800941554991114/812723546904199168/ytesportsREGCofficial.gif')
-  .setTimestamp()
-	.setFooter('Young Terminator Esports');
+	.setTitle('REGISTRATION CLOSED 🚧')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/Llu4eiV.png', 'https://www.youtube.com/channel/UCLt-31Xqvt5pbHVtfv5iPTw')
+	.setDescription('Subscription slots are available!!')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'PREMANENT FIXED SLOT', value: '' },
+		{ name: 'NO NEED TO REGISTER DAILY', value: '' },
+		{ name: 'NO NEED TO WAIT FOR SLOT LIST', value: '', inline: true },
+		{ name: 'YOU WILL GET A MEMBERSHIP ROLE FROM YT ESPORT', value: '', inline: true },
+	)
+	.addField('SUBSCRIBE TODAY', '', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Young Terminator Esports', 'https://i.imgur.com/Llu4eiV.png');
 
 message.channel.send(RegClosed);
-    message.channel.send(`To buy subscription slot(s) check <#812722317108576266>`)
+    message.channel.send(`**To Subcribe Fixed Slot Check** <#719936663005954088>`)
     message.delete()
   }
 }
