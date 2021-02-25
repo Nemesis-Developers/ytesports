60a86a1e6e3ca9e3460c342266e88d16eda22a12
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-25 13:33:18 +0530

    Update temprole.js

--- commands/moderation/temprole.js
+++ commands/moderation/temprole.js
@@ -1,36 +1,36 @@
 const { MessageEmbed } = require('discord.js')
 const ms = require('ms');
-
+const { Role } = require("discord.js")
 module.exports = {
   name: "tmr",
-  aliases: ["trole", "temprole"],
+  aliases: ["trole", "T!role"],
   category: "moderation",
   description: "Add role to any user",
-      run: async (client, message, args) => {
+  run: async (client, message, args) => {
    if (!message.member.hasPermission("MANAGE_ROLES")) {
       return message.channel.send("sorry you need permission to mute someone");
     }
     if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
       return message.channel.send("I do not have permission to mute");
     } 
+    
 let muteTime = ms(args[0]);
     
 const targets = message.mentions.members;
    
-    if(!targets) return message.reply(`Please mention user!`)
+    if(!targets) return message.reply(`<:no:677902165859237894>please mention user!`)
     
     let arole = message.mentions.roles.first();
     
-    if(!arole) return message.reply(`Please mention a role to add`)
-        
-    const highest = message.member.roles.highest; 
-if (Role.comparePositions(highest, arole) <= 0) return 
-        message.channel.send("The role You mention must be lower than the position of your highest role.\n **Bahut Tez Ho Rahe Ho**");
-     
-        const embed = new MessageEmbed()
+    if(!arole) return message.reply(`<:no:677902165859237894>please mention role for add!`)
+const highest = message.member.roles.highest; 
+if (Role.comparePositions(highest, arole) <= 0) 
+return message.channel.send(`The role You mention must be lower than the position of your highest role.\n **Bahut Tez Ho Rahe Ho**`);
+    
+      const embed = new MessageEmbed()
       
       .setColor("RANDOM")
-      .setDescription(`Temporarily added ${arole}`)
+      .setDescription(`<a:ok_:731369076315652167>role added ${arole} ${muteTime}`)
       
       await message.channel.send(embed)
       
