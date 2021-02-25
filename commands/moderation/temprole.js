838858de1b9720cf87c9116af6edd5fd9ffd01db
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-25 14:06:48 +0530

    Update temprole.js

--- commands/moderation/temprole.js
+++ commands/moderation/temprole.js
@@ -8,7 +8,7 @@ module.exports = {
   description: "Add role to any user",
   run: async (client, message, args) => {
    if (!message.member.hasPermission("MANAGE_ROLES")) {
-      return message.channel.send("sorry you need permission to mute someone");
+      return message.channel.send("Sorry you need permission");
     }
     if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
       return message.channel.send("I do not have permission");
@@ -18,7 +18,7 @@ let muteTime = ms(args[0]);
     
 const targets = message.mentions.members;
    
-    if(!targets) return message.reply(`<:no:677902165859237894>please mention user!`)
+    if(!targets) return message.reply(`<:no:677902165859237894> Please mention user!`)
     
     let arole = message.mentions.roles.first();
     
