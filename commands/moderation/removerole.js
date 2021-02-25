363d464ea1a711c89114154beb2f08435ee876e9
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-02-25 14:11:27 +0530

    Create removerole.js

---
+++ commands/moderation/removerole.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js')
+const { Role } = require("discord.js")
+module.exports = {
+  name: "addrole",
+  aliases: ["rr", "rrole"],
+  category: "moderation",
+  description: "Add role to any user",
+  run: async (client, message, args) => {
+   if (!message.member.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send("Sorry you need permission");
+    }
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send("I do not have permission");
+    } 
+    const targets = message.mentions.members;
+   
+    if(!targets) return message.reply(`<:no:677902165859237894> Please mention user!`)
+    
+    let arole = message.mentions.roles.first();
+    
+    if(!arole) return message.reply(`<:xmark:801109909030174760>  Please mention role!`)
+const highest = message.member.roles.highest; if (Role.comparePositions(highest, arole) <= 0) return message.channel.send(`The role You mention must be lower than the position of your highest role.\n **Bahut Tez Ho Rahe Ho**`);
+      const embed = new MessageEmbed()
+      
+      .setColor("RANDOM")
+      .setDescription(`<a:emoji_101:752199185348165782> Successfully added ${arole} to the user(S)`)
+      
+      await message.channel.send(embed)
+      
+    targets.forEach(target => target.roles.remove(arole));
+    
+  }
+}
