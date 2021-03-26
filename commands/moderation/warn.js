6baec324dc4a44c7e9e27f1560466a27607c8201
Anush Shetty <anushshetty90@gmail.com>
2021-03-26 21:46:30 +0530

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -58,7 +58,7 @@ message.delete()
       let embed = new MessageEmbed()
 .setColor('#0099ff')
 	.setTitle('**WARNED AGAIN!!**')
-	.setDescription(`${user} HAS BEEN WARNED AGAIN! ${reason}. Multiple violations will lead to server ban!`)
+	.setDescription(`${user} HAS BEEN WARNED AGAIN! ${reason}.`)
 	.setThumbnail('https://i.ibb.co/PCnBZ8w/IMG-20210205-191207.jpg')
 	.setFooter('Young Terminator eSports')
 	await message.channel.send(embed);
