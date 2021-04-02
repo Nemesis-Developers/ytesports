c30369cae0c0d6a59a1f24820e2fd1ad799e4824
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-04-02 11:20:54 +0530

    Create steal.js

---
+++ commands/moderation/steal.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const { parse } = require("twemoji-parser");
+
+
+module.exports = {
+  name: "emojiadd",
+aliases: ["steal", "addemoji"],
+  category: "moderation",
+  usage: "stealemoji <emoji> <custom name>",
+  description: "Steal an emoji from a different server",
+  botPermissions: ["MANAGE_EMOJIS"],
+  memberPermissions: ["MANAGE_EMOJIS"],
+  async run(bot, message, args) {
+    const emoji = args[0];
+    if (!emoji) return message.channel.send("Please Give Me A Emoji!");
+
+    let customemoji = Discord.Util.parseEmoji(emoji);
+
+    if (customemoji.id) {
+      const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
+        customemoji.animated ? "gif" : "png"
+      }`;
+      const name = args.slice(1).join(" ");
+
+      message.guild.emojis.create(
+        `${Link}`,
+        `${name || `${customemoji.name}`}`
+      );
+      const Added = new Discord.MessageEmbed()
+        .setTitle("Emoji Added")
+        .setColor("BLUE")
+        .setDescription(
+          `Emoji Has Been Added! | Name : ${
+            name || `${customemoji.name}`
+          } | Preview : [Click Me](${Link})`
+        );
+      return message.channel.send(Added);
+    } else {
+      let CheckEmoji = parse(emoji, { assetType: "png" });
+      if (!CheckEmoji[0])
+        return message.channel.send("Please Give Me A Valid Emoji!");
+      message.channel.send(
+        "You Can Use Normal Emoji Without Adding In Server!"
+      );
+    }
+  },
+};
