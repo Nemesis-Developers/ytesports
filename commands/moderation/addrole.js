a1600b766c33aac276ab2f93205ae458c5480456
Anush Shetty <anushshetty90@gmail.com>
2021-04-28 20:06:27 +0530

    Update addrole.js

--- commands/moderation/addrole.js
+++ commands/moderation/addrole.js
@@ -13,7 +13,7 @@ module.exports = {
       return message.channel.send("I do not have permission");
     } 
     const targets = message.mentions.members;
-   
+   const targets1 = message.mentions.members.size;
     if(!targets) return message.reply(`<:no:677902165859237894> Please mention user!`)
     
     let arole = message.mentions.roles.first();
@@ -22,8 +22,8 @@ module.exports = {
 const highest = message.member.roles.highest; if (Role.comparePositions(highest, arole) <= 0) return message.channel.send(`The role You mention must be lower than the position of your highest role.\n **Bahut Tez Ho Rahe Ho**`);
       const embed = new MessageEmbed()
       
-      .setColor("RANDOM")
-      .setDescription(`<a:emoji_101:752199185348165782> Successfully added ${arole} to the user(S)`)
+      .setColor("RED")
+      .setDescription(`<a:emoji_101:752199185348165782> Added ${arole} to ${targets1} members`)
       
       await message.channel.send(embed)
       
